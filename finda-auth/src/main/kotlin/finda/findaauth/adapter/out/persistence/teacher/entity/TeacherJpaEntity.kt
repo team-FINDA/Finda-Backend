8543f829f791097b6e3b75cf54cd405cdee72bac
dohwi <dohwe0978@gmail.com>
2026-01-22 15:57:29 +0900

    feat(#6): student

---
+++ finda-auth/src/main/kotlin/finda/findaauth/adapter/out/persistence/teacher/entity/TeacherJpaEntity.kt
@@ -0,0 +1,21 @@
+package finda.findaauth.adapter.out.persistence.teacher.entity
+
+import finda.findaauth.adapter.out.persistence.BaseEntity
+import finda.findaauth.adapter.out.persistence.user.entity.UserJpaEntity
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import java.util.UUID
+
+@Entity
+@Table(name = "tbl_teacher")
+class TeacherJpaEntity(
+    id: UUID?,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", unique = true)
+    val user: UserJpaEntity?
+
+) : BaseEntity(id)
