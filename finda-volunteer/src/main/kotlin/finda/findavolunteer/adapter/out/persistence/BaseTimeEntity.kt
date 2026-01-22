80df8a7704e124d5788a75e2137ed6d6624425b7
dohwi <dohwe0978@gmail.com>
2026-01-22 11:31:42 +0900

    feat(#6): base entity

---
+++ finda-volunteer/src/main/kotlin/finda/findavolunteer/adapter/out/persistence/BaseTimeEntity.kt
@@ -0,0 +1,19 @@
+package finda.findavolunteer.adapter.out.persistence
+
+import jakarta.persistence.Column
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import java.time.LocalDateTime
+
+@MappedSuperclass
+abstract class BaseTimeEntity(
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+
+    @LastModifiedDate
+    @Column(name = "modified_at")
+    var modifiedAt: LocalDateTime = LocalDateTime.now()
+
+)
