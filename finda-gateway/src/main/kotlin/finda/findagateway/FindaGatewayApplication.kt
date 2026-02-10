166bc6436226bca637c46d444f099468ebb46de0
박지민 <pjmlove2008@gmail.com>
2026-02-10 15:11:56 +0900

    chore(#11): 기본 타임존을 Asia/Seoul로 설정

--- finda-gateway/src/main/kotlin/finda/findagateway/FindaGatewayApplication.kt
+++ finda-gateway/src/main/kotlin/finda/findagateway/FindaGatewayApplication.kt
@@ -3,6 +3,7 @@ package finda.findagateway
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import java.util.TimeZone
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
@@ -10,4 +11,5 @@ class FindaGatewayApplication
 
 fun main(args: Array<String>) {
     runApplication<FindaGatewayApplication>(*args)
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
 }
