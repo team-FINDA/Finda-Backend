85a8cf899b26728924f102f3e3a05d9be08d11bb
dohwi <dohwe0978@gmail.com>
2026-02-18 21:55:28 +0900

    refactor(#19): 로깅 추가

--- finda-batch/src/main/kotlin/finda/findabatch/infra/event/producer/VolunteerRemindEventProducer.kt
+++ finda-batch/src/main/kotlin/finda/findabatch/infra/event/producer/VolunteerRemindEventProducer.kt
@@ -1,6 +1,7 @@
 package finda.findabatch.infra.event.producer
 
 import finda.findabatch.infra.event.dto.volunteer.VolunteerRemindFiredEvent
+import org.slf4j.LoggerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
 
@@ -8,10 +9,13 @@ import org.springframework.stereotype.Component
 class VolunteerRemindEventProducer(
     private val kafkaTemplate: KafkaTemplate<String, Any>
 ) {
+    private val log = LoggerFactory.getLogger(javaClass)
     private val TOPIC = "VOLUNTEER-REMINED-FIRED"
 
     // 같은 파티션 보장 필요 없음으로 key를 설정하지 않음
     fun produce(event: VolunteerRemindFiredEvent) {
-        kafkaTemplate.send(TOPIC, event)
+        kafkaTemplate.send(TOPIC, event).whenComplete { _, ex ->
+            if (ex != null) log.error("Failed to send VolunteerRemindFiredEvent: $event", ex)
+        }
     }
 }
--- finda-batch/src/main/kotlin/finda/findabatch/infra/event/producer/VolunteerStatusChangedEventProducer.kt
+++ finda-batch/src/main/kotlin/finda/findabatch/infra/event/producer/VolunteerStatusChangedEventProducer.kt
@@ -1,6 +1,7 @@
 package finda.findabatch.infra.event.producer
 
 import finda.findabatch.infra.event.dto.volunteer.VolunteerStatusChangedFiredEvent
+import org.slf4j.LoggerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
 
@@ -8,10 +9,12 @@ import org.springframework.stereotype.Component
 class VolunteerStatusChangedEventProducer(
     private val kafkaTemplate: KafkaTemplate<String, Any>
 ) {
-
+    private val log = LoggerFactory.getLogger(javaClass)
     private val TOPIC = "VOLUNTEER-STATUS-FIRED"
 
     fun produce(event: VolunteerStatusChangedFiredEvent) {
-        kafkaTemplate.send(TOPIC, event)
+        kafkaTemplate.send(TOPIC, event).whenComplete { _, ex ->
+            if (ex != null) log.error("Failed to send VolunteerStatusChangedFiredEvent: $event", ex)
+        }
     }
 }
