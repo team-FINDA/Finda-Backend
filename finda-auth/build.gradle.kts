f22f156b5f0add9e488ecbd397b5f51f2402afa8
박지민 <pjmlove2008@gmail.com>
2026-03-02 10:12:26 +0900

    chore(#27): Kotlin compilerOptions 추가 및 null 안정성 강화

--- finda-auth/build.gradle.kts
+++ finda-auth/build.gradle.kts
@@ -48,7 +48,7 @@ protobuf {
     }
     plugins {
         create("grpc") {
-            artifact = "io.grpc:protoc-gen-grpc-java:1.58.0"
+            artifact = "io.grpc:protoc-gen-grpc-java:1.79.0"
         }
     }
     generateProtoTasks {
@@ -62,4 +62,16 @@ protobuf {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+
+        freeCompilerArgs.addAll(
+            "-Xjsr305=strict",
+            "-Xjvm-default=all",
+            "-opt-in=kotlin.RequiresOptIn"
+        )
+    }
 }
--- finda-notification/build.gradle.kts
+++ finda-notification/build.gradle.kts
@@ -43,7 +43,7 @@ protobuf {
     }
     plugins {
         create("grpc") {
-            artifact = "io.grpc:protoc-gen-grpc-java:1.58.0"
+            artifact = "io.grpc:protoc-gen-grpc-java:1.79.0"
         }
     }
     generateProtoTasks {
