4aeb655dc7f7bf18dc2f0c049b99816b6f9d8068
dohwi <dohwe0978@gmail.com>
2026-03-03 18:51:27 +0900

    feat(#26): timestamp 추가

--- finda-security-common/src/main/kotlin/finda/security/passport/model/Passport.kt
+++ finda-security-common/src/main/kotlin/finda/security/passport/model/Passport.kt
@@ -5,5 +5,7 @@ import java.util.UUID
 data class Passport(
     val userId: UUID,
     val authority: Authority,
-    val userIntegrity: String
+    val userIntegrity: String,
+    val issuedAt: Long, // 발급 시간 (Unix timestamp in milliseconds)
+    val expiresAt: Long // 만료 시간 (Unix timestamp in milliseconds)
 )
