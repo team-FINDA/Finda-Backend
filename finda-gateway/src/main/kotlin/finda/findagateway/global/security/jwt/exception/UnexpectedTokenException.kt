ae89f3cd83be20119154cb02f6955d7f3ab3357c
dohwi <dohwe0978@gmail.com>
2026-03-03 19:51:40 +0900

    chore(#26): security 삭제

--- finda-gateway/src/main/kotlin/finda/findagateway/global/security/jwt/exception/UnexpectedTokenException.kt
+++
@@ -1,7 +0,0 @@
-package finda.findagateway.global.security.jwt.exception
-
-import finda.findagateway.global.error.exception.ErrorCode
-import finda.findagateway.global.error.exception.JwtAuthenticationException
-
-object UnexpectedTokenException :
-    JwtAuthenticationException(ErrorCode.UNEXPECTED_TOKEN)
