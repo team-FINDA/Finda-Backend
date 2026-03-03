0c5cd39845ba427d268ba333505435859cf4d6aa
dohwi <dohwe0978@gmail.com>
2026-03-03 11:28:22 +0900

    feat(#26): security

---
+++ finda-security-common/src/main/kotlin/finda/security/path/SecurityPath.kt
@@ -0,0 +1,14 @@
+package finda.security.path
+
+object SecurityPath {
+    val PERMIT_ALL_PATHS = listOf(
+        "/auth/reissue",
+        "/students/signup",
+        "/students/login",
+        "/teachers/signup",
+        "/teachers/login",
+        "/students/send-verification",
+        "/students/verify-email",
+        "/email/**"
+    )
+}
---
+++ finda-security-common/src/main/kotlin/finda/security/properties/SecurityObject.kt
@@ -0,0 +1,9 @@
+package finda.security.properties
+
+object SecurityObject {
+    val CLAIM_TYPE = "type"
+    val TOKEN_TYPE_ACCESS = "access"
+    val TOKEN_TYPE_REFRESH = "refresh"
+    val CLAIM_USER_TYPE = "userType"
+    val MILLIS_PER_SECOND = 1000L
+}
