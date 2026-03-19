a5983108d072b3e0515b24dcc169b5a02d01fb5c
박지민 <pjmlove2008@gmail.com>
2026-03-19 10:48:22 +0900

    fix(#31): passport 객체를 직접 사용하지 않도록 수정

--- finda-notification/src/main/kotlin/finda/findanotification/application/port/in/notice/CreateNoticeUseCase.kt
+++ finda-notification/src/main/kotlin/finda/findanotification/application/port/in/notice/CreateNoticeUseCase.kt
@@ -1,8 +1,7 @@
 package finda.findanotification.application.port.`in`.notice
 
 import finda.findanotification.application.port.`in`.notice.dto.request.NoticeCommand
-import finda.security.passport.model.Passport
 
 interface CreateNoticeUseCase {
-    fun execute(request: NoticeCommand, passport: Passport)
+    fun execute(request: NoticeCommand)
 }
