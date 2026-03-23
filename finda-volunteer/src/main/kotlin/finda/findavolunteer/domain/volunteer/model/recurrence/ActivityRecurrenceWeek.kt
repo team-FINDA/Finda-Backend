b06e024da8d70e93b178efd31c10f5e7616e6a2a
dohwi <dohwe0978@gmail.com>
2026-03-23 11:24:42 +0900

    chore(#33): model 객체 id 디폴트 0으로된 uuid로 변경

--- finda-volunteer/src/main/kotlin/finda/findavolunteer/domain/volunteer/model/recurrence/ActivityRecurrenceWeek.kt
+++ finda-volunteer/src/main/kotlin/finda/findavolunteer/domain/volunteer/model/recurrence/ActivityRecurrenceWeek.kt
@@ -4,7 +4,7 @@ import finda.findavolunteer.domain.volunteer.enum.Weekday
 import java.util.UUID
 
 data class ActivityRecurrenceWeek(
-    val id: UUID,
+    val id: UUID = UUID(0, 0),
     val weekday: Weekday,
     val volunteerId: UUID
 )
