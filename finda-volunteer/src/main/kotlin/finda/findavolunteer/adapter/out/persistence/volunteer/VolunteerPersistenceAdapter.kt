a595a3e0e700d2aa02c9d8896830a33131cd9f1a
dohwi <dohwe0978@gmail.com>
2026-03-24 19:16:39 +0900

    feat(#33): volunteer query adapter

--- finda-volunteer/src/main/kotlin/finda/findavolunteer/adapter/out/persistence/volunteer/VolunteerPersistenceAdapter.kt
+++ finda-volunteer/src/main/kotlin/finda/findavolunteer/adapter/out/persistence/volunteer/VolunteerPersistenceAdapter.kt
@@ -6,11 +6,16 @@ import finda.findavolunteer.adapter.out.persistence.volunteer.mapper.VolunteerMa
 import finda.findavolunteer.adapter.out.persistence.volunteer.repository.ActivityRecurrenceMonthRepository
 import finda.findavolunteer.adapter.out.persistence.volunteer.repository.ActivityRecurrenceWeekRepository
 import finda.findavolunteer.adapter.out.persistence.volunteer.repository.VolunteerRepository
+import finda.findavolunteer.application.exception.VolunteerNotFoundException
 import finda.findavolunteer.application.port.out.volunteer.VolunteerCommandPort
+import finda.findavolunteer.application.port.out.volunteer.VolunteerQueryPort
 import finda.findavolunteer.domain.volunteer.model.Volunteer
 import finda.findavolunteer.domain.volunteer.model.recurrence.ActivityRecurrenceMonth
 import finda.findavolunteer.domain.volunteer.model.recurrence.ActivityRecurrenceWeek
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
+import java.util.*
+import kotlin.jvm.optionals.getOrNull
 
 @Component
 class VolunteerPersistenceAdapter(
@@ -20,7 +25,7 @@ class VolunteerPersistenceAdapter(
     val activityRecurrenceMonthMapper: ActivityRecurrenceMonthMapper,
     val activityRecurrenceWeekRepository: ActivityRecurrenceWeekRepository,
     val activityRecurrenceWeekMapper: ActivityRecurrenceWeekMapper
-) : VolunteerCommandPort {
+) : VolunteerCommandPort, VolunteerQueryPort {
     override fun save(volunteer: Volunteer): Volunteer {
         val entity = volunteerRepository.save(volunteerMapper.toEntity(volunteer))
         return volunteerMapper.toDomain(entity)
@@ -34,5 +39,22 @@ class VolunteerPersistenceAdapter(
     override fun saveMonthRecurrence(activityRecurrenceMonth: ActivityRecurrenceMonth): ActivityRecurrenceMonth {
         val entity = activityRecurrenceMonthRepository.save(activityRecurrenceMonthMapper.toEntity(activityRecurrenceMonth))
         return activityRecurrenceMonthMapper.toDomain(entity)
+    }
+
+    override fun findByUserId(userId: UUID): List<Volunteer> {
+        TODO("Not yet implemented")
+    }
+
+    override fun findById(id: UUID): Volunteer? {
+        val entity = volunteerRepository.findByIdOrNull(id)
+        return entity?.let{ volunteerMapper.toDomain(entity) }
+    }
+
+    override fun findByIdOrThrow(id: UUID): Volunteer {
+        return findById(id) ?: throw VolunteerNotFoundException
+    }
+
+    override fun deleteById(volunteerId: UUID) {
+        volunteerRepository.deleteById(volunteerId)
     }
 }
