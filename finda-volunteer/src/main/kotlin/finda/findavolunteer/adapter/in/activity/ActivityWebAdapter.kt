1dfccad0cb141c5c7057c7e857d5fa6a586b86bc
변도휘 <dohwe0978@gmail.com>
2026-04-02 11:14:07 +0900

    fix(#41): 유저 활동 생성 요청 바디 매핑 추가

--- finda-volunteer/src/main/kotlin/finda/findavolunteer/adapter/in/activity/ActivityWebAdapter.kt
+++ finda-volunteer/src/main/kotlin/finda/findavolunteer/adapter/in/activity/ActivityWebAdapter.kt
@@ -3,6 +3,7 @@ package finda.findavolunteer.adapter.`in`.activity
 import finda.findavolunteer.adapter.`in`.activity.dto.request.CreateUserActivityRequest
 import finda.findavolunteer.application.port.`in`.activity.CreateUserActivityUseCase
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,5 +13,5 @@ class ActivityWebAdapter(
     private val createUserActivityUseCase: CreateUserActivityUseCase
 ) {
     @PostMapping("/user")
-    fun createUserActivity(request: CreateUserActivityRequest) = createUserActivityUseCase.execute(request)
+    fun createUserActivity(@RequestBody request: CreateUserActivityRequest) = createUserActivityUseCase.execute(request)
 }
