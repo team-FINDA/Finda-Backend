52d1ba7df16436a41c078f31b1e7ef5e6ceab6ae
dohwi <dohwe0978@gmail.com>
2026-04-12 20:58:23 +0900

    fix: teacher 참여 컬럼 유니크 제약 삭제

--- finda-volunteer/src/main/resources/db/changelog-volunteer/schema/003-add-unique-teacher-participation-user-id.sql
+++
@@ -1,5 +0,0 @@
---liquibase formatted sql
-
---changeset byeondohwi:003-add-unique-teacher-participation-user-id
-ALTER TABLE tbl_teacher_participation
-    ADD CONSTRAINT uk_teacher_participation_user_id UNIQUE (user_id);
