66f0b876cd2589795b0068cd98ada98c65848f01
dohwi <dohwe0978@gmail.com>
2026-04-19 22:06:43 +0900

    chore(#51): 반환 Status 추가

--- finda-volunteer/src/main/kotlin/finda/findavolunteer/adapter/in/qrcode/QrCodeWebAdapter.kt
+++ finda-volunteer/src/main/kotlin/finda/findavolunteer/adapter/in/qrcode/QrCodeWebAdapter.kt
@@ -7,9 +7,11 @@ import finda.findavolunteer.adapter.`in`.qrcode.mapper.toCommand
 import finda.findavolunteer.application.service.qrcode.AttendanceQrCodeService
 import finda.findavolunteer.application.service.qrcode.AttendanceStudentService
 import finda.findavolunteer.application.service.qrcode.CreateQrCodeService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -20,14 +22,17 @@ class QrCodeWebAdapter(
     private val attendanceStudentService: AttendanceStudentService
 ) {
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun createQrCode(@RequestBody request: CreateQrCodeRequest) =
         createQrCodeService.execute(request.toCommand())
 
     @PostMapping("/attendance")
+    @ResponseStatus(HttpStatus.OK)
     fun attendByQrCode(@RequestBody request: AttendanceQrCodeRequest) =
         attendanceQrCodeService.execute(request.toCommand())
 
     @PostMapping("/attendance/students")
+    @ResponseStatus(HttpStatus.OK)
     fun attendStudents(@RequestBody request: AttendanceStudentsRequest) =
         attendanceStudentService.execute(request.toCommand())
 }
