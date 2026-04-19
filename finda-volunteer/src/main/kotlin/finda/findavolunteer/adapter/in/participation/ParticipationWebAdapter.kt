655f09fb4a93cddf6009032e0d74f66c36b1ea92
dohwi <dohwe0978@gmail.com>
2026-04-19 23:03:58 +0900

    chore(#51): 반환 status 추가

--- finda-volunteer/src/main/kotlin/finda/findavolunteer/adapter/in/participation/ParticipationWebAdapter.kt
+++ finda-volunteer/src/main/kotlin/finda/findavolunteer/adapter/in/participation/ParticipationWebAdapter.kt
@@ -3,8 +3,10 @@ package finda.findavolunteer.adapter.`in`.participation
 import finda.findavolunteer.adapter.`in`.participation.dto.request.CreateQrCodeRequest
 import finda.findavolunteer.adapter.`in`.participation.mapper.toCommand
 import finda.findavolunteer.application.service.qrcode.CreateQrCodeService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -13,5 +15,6 @@ class ParticipationWebAdapter(
     private val createQrCodeService: CreateQrCodeService
 ) {
     @PostMapping("/qr/create")
+    @ResponseStatus(HttpStatus.CREATED)
     fun createQr(request: CreateQrCodeRequest) = createQrCodeService.execute(request.toCommand())
 }
