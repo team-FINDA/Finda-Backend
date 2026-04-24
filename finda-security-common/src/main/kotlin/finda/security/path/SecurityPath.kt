b5b5160d0e5956abd294865a7fce52c06d6b89f7
FIRE-918 <pjmlove2008@gmail.com>
2026-04-24 10:06:56 +0900

    chore: update config

--- finda-security-common/src/main/kotlin/finda/security/path/SecurityPath.kt
+++ finda-security-common/src/main/kotlin/finda/security/path/SecurityPath.kt
@@ -2,16 +2,12 @@ package finda.security.path
 
 object SecurityPath {
     val PERMIT_ALL_PATHS = listOf(
-        "/auth/test",
         "/auth/reissue",
         "/students/signup",
         "/students/login",
         "/teachers/signup",
         "/teachers/login",
-        "/teachers/verify",
-        "/teachers/verify-email",
         "/students/send-verification",
-        "/teachers/send-verification",
         "/students/verify-email",
         "/email/**"
     )
