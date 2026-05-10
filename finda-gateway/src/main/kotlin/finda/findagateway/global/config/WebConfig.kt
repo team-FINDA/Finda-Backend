fac6be4c60031d0020236745e096842f8db159f1
dohwi <dohwe0978@gmail.com>
2026-05-10 22:59:10 +0900

    feat(#76): cors를 webflux 환경에서 작동할 수 있게 변경

--- finda-gateway/src/main/kotlin/finda/findagateway/global/config/WebConfig.kt
+++ finda-gateway/src/main/kotlin/finda/findagateway/global/config/WebConfig.kt
@@ -1,17 +1,27 @@
 package finda.findagateway.global.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 
 @Configuration
-class WebConfig : WebMvcConfigurer {
-    override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")
-            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-            // 프론트 배포 후에 변경
-            .allowedOrigins("*")
-            .exposedHeaders(HttpHeaders.CONTENT_DISPOSITION)
+class WebConfig {
+
+    @Bean
+    fun corsWebFilter(): CorsWebFilter {
+        val config = CorsConfiguration()
+        config.addAllowedMethod("*")
+        // 프론트 배포 후에 변경
+        config.addAllowedOrigin("*")
+        config.addExposedHeader(HttpHeaders.CONTENT_DISPOSITION)
+        config.addAllowedHeader("*")
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", config)
+
+        return CorsWebFilter(source)
     }
 }
